cba052f6b4f8736bc95cd7dba5509940197692b9
unknown <nikoladimitrijevic1991@gmail.com>
2019-04-19 01:13:07 +0200

    asdd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import MyHeader from './assets/components/MyHeader.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      asd
+        <MyHeader />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/assets/components/MyHeader.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class MyHeader extends Component{
+  render(){
+    return(
+      <div>
+        <p>CCC</p>
+      </div>
+    )
+  }
+}
+
+export default MyHeader;
