e9538df35c3f8b876676bd49164859e87f810c00
B0jan93 <samo.bojan933@gmail.com>
2019-04-18 16:20:59 -0700

    zor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MyHeader from './assets/components/MyHeader.js'
+import MyFooter from './assets/components/MyFooter.js'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <MyHeader />
+        <MyFooter></MyFooter>
       </div>
     );
   }
---
+++ src/assets/components/MyFooter.js
@@ -0,0 +1,14 @@
+
+import React,{Component} from 'react';
+
+class MyFooter extends Component{
+  render(){
+    return(
+      <div>
+        <b><p>lorem ipsonologenelogion</p></b>
+      </div>
+    )
+  }
+}
+
+export default MyFooter;
